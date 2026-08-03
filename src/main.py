89802eec8887eaa4ca16388f865e6ea89f22bc0b
MollyChen11 <mollyyhchen@gmail.com>
2026-08-03 10:48:17 -0700

    Initial project setup

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import librosa
+from pathlib import Path
+
+def main():
+    print("Music Transcriber Started!")
+
+    project_root = Path(__file__).parent.parent
+    audio_path = project_root/"audio"/"flute_note.wav"
+
+    audio_data, sample_rate = librosa.load(audio_path)
+
+    print(f"Sample rate: {sample_rate}")
+    print(f"Number of samples: {len(audio_data)}")
+
+if __name__ == "__main__":
+    main()
